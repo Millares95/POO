d8316f75b3eb04c0440eeb1df3c5ba050802f4ea
Millares95 <millareslazaro1@gmail.com>
2024-09-24 11:37:43 +0200

    organizando los scrips del libro

--- Circle.py
+++ Circle.py
@@ -2,7 +2,7 @@ class Circle :
     def perimeter (self):
         # Definicion del cuerpo del metodo,
         #em la ocurrencia con un valor de retorno 
-        return 2*3.14 *self.radio
+        return 2 * 3.14 * self.radio
 
 c = Circle()
 c.radio = 2
@@ -12,5 +12,7 @@ p= Circle.perimeter
 # p = c.perimeter
 print(p(c))
 
-# r= Circle()
-# r.radio = 4 
+r = Circle()
+r.radio = 4 
+print(r.perimeter())
+
